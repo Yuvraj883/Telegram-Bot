57b8f2e7e6024645b526c0ceb4208dfd1d504be5
Yuvraj <kingyuvi88@gmail.com>
2023-04-02 13:25:36 +0530

    directly providing token

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const telegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 
 // const TOKEN = process.env.TOKEN;
-const TOKEN = 6101609484:AAEn1Jd1i4AkNwvS1hU06kRK2P9cRs0l84g; 
+const TOKEN = '6101609484:AAEn1Jd1i4AkNwvS1hU06kRK2P9cRs0l84g'; 
 
 const bot = new telegramBot(TOKEN, {polling:true});
 
