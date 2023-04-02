13a09d2e2652d2f627bd7517aef78e11dcadc882
Yuvraj <kingyuvi88@gmail.com>
2023-04-02 13:49:27 +0530

    directly providing token

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const TOKEN = '6101609484:AAEn1Jd1i4AkNwvS1hU06kRK2P9cRs0l84g';
 const bot = new telegramBot(TOKEN, {polling:true});
 
 bot.on('message',(message)=>{
-    // console.log(message.text); 
+     console.log(message.text); 
     // console.log(message.from.id);
     let chatId = message.from.id; 
     bot.sendMessage(chatId, "Hello Nigga!!");
