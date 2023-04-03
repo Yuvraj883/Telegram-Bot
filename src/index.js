892a77be64f99f99475da9bd4d4e9c6e3778e612
Yuvraj <kingyuvi88@gmail.com>
2023-04-03 20:37:48 +0530

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const telegramBot = require('node-telegram-bot-api'); 
 require('dotenv').config();
+const PORT = process.env.PORT || 3030;
+// const express = require('express');
+// const app = express();
 
+// app.listen(PORT,()=>{
+// console.log(`App started at the PORT ${PORT}`);
+// })
 // const TOKEN = process.env.TOKEN;
 const TOKEN = '6101609484:AAEn1Jd1i4AkNwvS1hU06kRK2P9cRs0l84g'; 
 
