cd244b590286331776d65a02ca33e3c9b5ed0bb1
Yuvraj <kingyuvi88@gmail.com>
2023-04-04 23:51:05 +0530

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ const PORT = process.env.PORT || 3030;
 // app.listen(PORT,()=>{
 // console.log(`App started at the PORT ${PORT}`);
 // })
-// const TOKEN = process.env.TOKEN;
-const TOKEN = '6101609484:AAEn1Jd1i4AkNwvS1hU06kRK2P9cRs0l84g'; 
+ const TOKEN = process.env.TOKEN;
+// const TOKEN = '6101609484:AAEn1Jd1i4AkNwvS1hU06kRK2P9cRs0l84g'; 
 
 const bot = new telegramBot(TOKEN, {polling:true});
 
